85b576f3d450e76bca08e5ffd51df8cf0e31a718
dzonib <nikolabosnjak381@gmail.com>
2018-08-07 20:28:55 +0200

    referencing done, embeding next

--- population.js
+++ population.js
@@ -1,24 +1,38 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose
+  .connect(
+    'mongodb://test:test135@ds115442.mlab.com:15442/document-reference',
+    { useNewUrlParser: true }
+  )
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
-const Author = mongoose.model('Author', new mongoose.Schema({
-  name: String,
-  bio: String,
-  website: String
-}));
+const Author = mongoose.model(
+  'Author',
+  new mongoose.Schema({
+    name: String,
+    bio: String,
+    website: String
+  })
+);
 
-const Course = mongoose.model('Course', new mongoose.Schema({
-  name: String,
-}));
+const Course = mongoose.model(
+  'Course',
+  new mongoose.Schema({
+    name: String,
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Author'
+    }
+  })
+);
 
-async function createAuthor(name, bio, website) { 
+async function createAuthor(name, bio, website) {
   const author = new Author({
-    name, 
-    bio, 
-    website 
+    name,
+    bio,
+    website
   });
 
   const result = await author.save();
@@ -27,23 +41,25 @@ async function createAuthor(name, bio, website) {
 
 async function createCourse(name, author) {
   const course = new Course({
-    name, 
+    name,
     author
-  }); 
-  
+  });
+
   const result = await course.save();
   console.log(result);
 }
 
-async function listCourses() { 
+async function listCourses() {
   const courses = await Course
-    .find()
-    .select('name');
+  .find()
+  .populate('author', 'name -_id')
+  // gives us access to author collection, it shows name and exclude id
+  .select('name, author');
   console.log(courses);
 }
 
-createAuthor('Mosh', 'My bio', 'My Website');
+// createAuthor('Mosh', 'My bio', 'My Website');
 
-// createCourse('Node Course', 'authorId')
+// createCourse('Node Course', "5b69dcc51718dc3d60f068a5")
 
-// listCourses();
+listCourses();
